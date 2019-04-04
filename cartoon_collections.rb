934519de2a7d1540cffc45a28317c9aa5a175093
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-04 06:03:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ calls_long.any? {|call| call.length > 4 }
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
+  strings.detect{|strings| cheese_types.include? strings}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
