0fe5308ea9fa31fd956088e2a22d5be1c59d2f74
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-04 05:39:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+planeteer_calls = ["earth", "wind", "fire"]
   planeteer_calls.map { |name| name.capitalize + "!"}
 end
 
